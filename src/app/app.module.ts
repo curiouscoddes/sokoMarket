15cbec1cfb325d12b2d6f9ec252cfb6f985251fd
curiouscoddes <curiouscoddes.com>
2018-08-15 10:08:58 +0300

    added all the new pages to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { OrdersPage} from '../pages/orders/orders';
+import {ChatPage} from '../pages/chat/chat';
+import {LocationPage} from '../pages/location/location';
+import {UserPage} from '../pages/user/user';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    OrdersPage,
+    ChatPage,
+    LocationPage,
+    UserPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +30,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    OrdersPage,
+    ChatPage,
+    LocationPage,
+    UserPage,
     TabsPage
   ],
   providers: [
